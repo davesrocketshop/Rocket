d4ee2fd81a8e81c52563de06feb3e9cc7e1d3777
David Carter <dcarter@davidcarter.ca>
2023-03-19 06:18:52 -0400

    New tests

--- TestRocketApp.py
+++ TestRocketApp.py
@@ -31,6 +31,7 @@ from Tests.TestNoses import NoseTests
 from Tests.TestTransition import TransitionTests
 from Tests.TestFlutter import FinFlutterTestCases
 from Tests.TestFins import FinTests
+# from Tests.TestFinCans import FinCanTests
 from Tests.Components.RocketTest import RocketTest
 from Tests.Components.PositionTests import PositionTests
 from Tests.TestMoves import MoveTests
