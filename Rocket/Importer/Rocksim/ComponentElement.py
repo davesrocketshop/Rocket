6893a15ebf49a59c8e77ff33a1879b028501a8ce
David Carter <dcarter@davidcarter.ca>
2025-01-05 07:31:25 -0500

    Now in color!

--- Rocket/Importer/Rocksim/ComponentElement.py
+++ Rocket/Importer/Rocksim/ComponentElement.py
@@ -44,22 +44,19 @@ class ComponentElement(Element):
     def __init__(self, parent, tag, attributes, parentObj, filename, line):
         super().__init__(parent, tag, attributes, parentObj, filename, line)
 
-        self._componentTags = []
-        self._knownTags = ["name", "knownmass", "density", "knowncg", "useknowncg", "densitytype", "texture", "ambient",
-                           "diffuse", "specular", "abientcolor", "diffusecolor", "specularcolor", "usesinglecolor",
-                           "simplecolormodel", "color", "finishcode", "material", "partmfg", "partno", "partdesc",
-                           "calcmass", "calccg", "wettedsurface", "paintedsurface", "radialloc", "radialangle", "opacity",
-                           "specularpower", "serialno", "displayflags", "metricsflags", "locationmode", "producetemplate",
-                           "templateunits", "removed", "station", "gluejointlength", "xb", "barrowmancna", "barrowmanxn",
-                           "rocksimcna", "rocksimxn", "len"]
+        self._componentTags = ["name", "len", "xb", "locationmode", "abientcolor", "diffusecolor", "specularcolor",
+                               "usesinglecolor", "simplecolormodel", "opacity"]
+        self._knownTags = ["knownmass", "density", "knowncg", "useknowncg", "densitytype", "texture", "ambient",
+                           "diffuse", "specular", "color", "finishcode", "material", "partmfg", "partno", "partdesc",
+                           "calcmass", "calccg", "wettedsurface", "paintedsurface", "radialloc", "radialangle",
+                           "specularpower", "serialno", "displayflags", "metricsflags", "producetemplate",
+                           "templateunits", "removed", "station", "gluejointlength", "barrowmancna", "barrowmanxn",
+                           "rocksimcna", "rocksimxn"]
 
         self._knownMass = 0
         self._knownCG = 0
         self._density = 0
         self._densityType = ""
-        self._ambient = 0
-        self._diffuse = 0
-        self._specular = 0
         self._ambientColor = 0
         self._diffuseColor = 0
         self._specularColor = 0
@@ -71,43 +68,16 @@ class ComponentElement(Element):
         self._materialType = None
         self._materialDensity = None
 
-    def handleTag(self, tag, attributes):
-        _tag = tag.lower().strip()
-        # if _tag == "position":
-        #     positionType = attributes["type"]
-        #     if positionType == "after":
-        #         self.onPositionType(LOCATION_AFTER)
-        #     elif positionType == "top":
-        #         self.onPositionType(LOCATION_PARENT_TOP)
-        #     elif positionType == "middle":
-        #         self.onPositionType(LOCATION_PARENT_MIDDLE)
-        #     elif positionType == "bottom":
-        #         self.onPositionType(LOCATION_PARENT_BOTTOM)
-        #     else:
-        #         self.onPositionType(LOCATION_BASE)
-        # elif _tag == "axialoffset":
-        #     positionType = attributes["method"]
-        #     if positionType == "after":
-        #         self.onPositionType(LOCATION_AFTER)
-        #     elif positionType == "top":
-        #         self.onPositionType(LOCATION_PARENT_TOP)
-        #     elif positionType == "middle":
-        #         self.onPositionType(LOCATION_PARENT_MIDDLE)
-        #     elif positionType == "bottom":
-        #         self.onPositionType(LOCATION_PARENT_BOTTOM)
-        #     else:
-        #         self.onPositionType(LOCATION_BASE)
-        # elif _tag == "material":
-        #     self._materialType = attributes["type"]
-        #     self._materialDensity = attributes["density"]
-        super().handleTag(tag, attributes)
+        self._appearance = FreeCAD.Material()
+
+    # def handleTag(self, tag, attributes):
+    #     _tag = tag.lower().strip()
+    #     super().handleTag(tag, attributes)
 
     def handleEndTag(self, tag, content):
         _tag = tag.lower().strip()
         if _tag == "name":
             self.onName(content)
-        elif _tag == "color":
-            self.onColor(content)
         elif _tag == "len":
             print("\tlen {}".format(float(content)))
             self.onLength(float(content))
@@ -115,6 +85,19 @@ class ComponentElement(Element):
             self.onAxialOffset(float(content))
         elif _tag == "locationmode":
             self.onLocationMode(content)
+        elif _tag == "abientcolor":
+            self._appearance.AmbientColor = self.parseColor(content)
+        elif _tag == "diffusecolor":
+            self._appearance.DiffuseColor = self.parseColor(content)
+        elif _tag == "specularcolor":
+            self._appearance.SpecularColor = self.parseColor(content)
+        elif _tag == "usesinglecolor":
+            pass
+        elif _tag == "simplecolormodel":
+            pass
+        elif _tag == "opacity":
+            self._appearance.Transparency = 1.0 - float(content)
+            pass
         else:
             super().handleEndTag(tag, content)
 
@@ -189,3 +172,28 @@ class ComponentElement(Element):
             self._feature._obj.ShapeMaterial = materialManager.getMaterial(uuid)
         except MaterialNotFoundError:
             _err(translate("Rocket", "Material '{}' not found - using default material").format(content))
+
+    def parseColor(self, value):
+        if value == "blue":
+            return (0, 0, 255)
+        elif value == "white":
+            return (255, 255, 255)
+        elif value == "red":
+            return (255, 0, 0)
+        elif value == "green":
+            return (0, 255, 0)
+        elif value == "black":
+            return (0, 0, 0)
+        elif(value.startswith("rgb(")):
+            color = value.removeprefix("rgb(").removesuffix(")")
+            print("Color({})".format(color))
+            t = tuple(int(x) for x in color.split(","))
+            print(t)
+            return t
+        return (255, 255, 255)
+
+    def end(self):
+        if hasattr(self._feature._obj.ViewObject, "ShapeAppearance"):
+            self._feature._obj.ViewObject.ShapeAppearance = self._appearance
+
+        return super().end()
