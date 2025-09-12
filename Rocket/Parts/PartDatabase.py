794bd6e496babaa21509ee70110fbed9b56acbb6
David Carter <dcarter@davidcarter.ca>
2025-09-12 10:30:09 -0400

    Updated part database

--- Rocket/Parts/PartDatabase.py
+++ Rocket/Parts/PartDatabase.py
@@ -151,7 +151,7 @@ class PartDatabase:
                 if not file.endswith(".sql"):
                     self._importOrcPartFile(connection, dirpath + file)
 
-        for (dirpath, dirnames, filenames) in walk(self._rootFolder + "/Resources/parts/openrocket-dbcook/orc/"):
+        for (dirpath, dirnames, filenames) in walk(self._rootFolder + "/Resources/parts/openrocket-database/orc/"):
             self._importOrcPartFile(connection, dirpath + 'generic_materials.orc')
             for file in filenames:
                 self._importOrcPartFile(connection, dirpath + file)
