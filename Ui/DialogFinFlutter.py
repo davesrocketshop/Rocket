48935a1efad38a13beb297a3085207faadfd3300
David Carter <dcarter@davidcarter.ca>
2025-12-03 04:22:18 -0500

    Graph in imperial units

--- Ui/DialogFinFlutter.py
+++ Ui/DialogFinFlutter.py
@@ -170,8 +170,12 @@ class DialogFinFlutter(UiDialog):
         self._flutterLine, = self._static_ax.plot(t, t, label=translate('Rocket', "Flutter"))
         self._divergenceLine, = self._static_ax.plot(t, t, label=translate('Rocket', "Divergence"))
         self._cursorLine, = self._static_ax.plot([0,0], [0,0])
-        self._static_ax.set_xlabel("Altitude (km)")
-        self._static_ax.set_ylabel("Velocity (m/s)")
+        if self._isMetricUnitPref():
+            self._static_ax.set_xlabel(translate('Rocket', "Altitude (km AGL)"))
+            self._static_ax.set_ylabel(translate('Rocket', "Velocity (m/s)"))
+        else:
+            self._static_ax.set_xlabel(translate('Rocket', "Altitude (x1000 ft AGL)"))
+            self._static_ax.set_ylabel(translate('Rocket', "Velocity (ft/s)"))
         self._static_ax.grid(visible=True)
         # self._static_ax.set_title("Flutter")
         self._static_ax.legend()
@@ -232,9 +236,17 @@ class DialogFinFlutter(UiDialog):
             return temperature + 273.15
         return temperature
 
+    def _getModulus(self) -> float:
+        return float(FreeCAD.Units.Quantity(str(self._ui.shearInput.text())))
+
+    def _formatFloat(self, value : float, units : str, metric : str, imperial : str) -> str:
+        quantity = FreeCAD.Units.Quantity(f"{value:.8g} {units}")
+        return f"{float(quantity.getValueAs(FreeCAD.Units.Quantity(metric))):.4f} {metric}, " \
+            f"{float(quantity.getValueAs(FreeCAD.Units.Quantity(imperial))):.4f} {imperial}"
+
     def _setSeries(self) -> None:
 
-        modulus = float(FreeCAD.Units.Quantity(str(self._ui.shearInput.text())))
+        modulus = self._getModulus()
         maxHeight = int(FreeCAD.Units.Quantity(self._ui.maxAltitudeCombo.currentText()).getValueAs(FreeCAD.Units.Quantity(self._heightUnits())) / 1000)
         launchHeight = float(FreeCAD.Units.Quantity(self._ui.launchSiteAltitudeInput.text())) / 1000.0 # in meters
 
@@ -249,21 +261,25 @@ class DialogFinFlutter(UiDialog):
         flutterSeries = []
         divergenceSeries = []
         for i in range(0, maxHeight+1):
-            altitude = i * 1000.0 # to m
+            altitude = FreeCAD.Units.Quantity(f"{float(i * 1000.0)} {self._heightUnits()}").Value / 1000.0 # to m
             flutter = self._flutter.flutterPOF615(atmosphericModel, altitude, modulus, launchHeight, launchTemperature)
             divergence = self._flutter.divergence(atmosphericModel, altitude, modulus, launchHeight, launchTemperature)
             # Getting the data
             x = float(i)
             x_axis.append(x)
 
-            flutterY = float(flutter[1])
+            quantity = FreeCAD.Units.Quantity(f"{float(flutter[1])} m/s")
+            flutterY = quantity.getValueAs(FreeCAD.Units.Quantity(self._velocityUnits())).Value
+            print(f"Altitude {self._formatFloat(altitude, 'm', 'm', 'ft')} -> {flutterY}")
+            print(f"Flutter {self._formatFloat(float(flutter[1]), 'm/s', 'm/s', 'ft/s')} -> {flutterY}")
             if x >= 0:
                 if (flutterY >=0):
                     flutterSeries.append(flutterY)
                 else:
                     flutterSeries.append(0)
 
-            divergenceY = float(divergence[1])
+            quantity = FreeCAD.Units.Quantity(f"{float(divergence[1])} m/s")
+            divergenceY = quantity.getValueAs(FreeCAD.Units.Quantity(self._velocityUnits())).Value
             if x >= 0:
                 if (divergenceY >=0):
                     divergenceSeries.append(divergenceY)
@@ -503,11 +519,11 @@ class DialogFinFlutter(UiDialog):
     def updateFlutter(self) -> None:
         self._graphFlutter()
         try:
-            modulus = float(FreeCAD.Units.Quantity(str(self._ui.shearInput.text())))
-            speed = float(FreeCAD.Units.Quantity(str(self._ui.speedInput.text())))
+            modulus = self._getModulus()
+            speed = float(FreeCAD.Units.Quantity(self._ui.speedInput.text()).Value)
 
             # Heights in meters
-            altitude = float(FreeCAD.Units.Quantity(str(self._ui.altitudeInput.text()))) / 1000.0
+            altitude = float(FreeCAD.Units.Quantity(self._ui.altitudeInput.text()).Value) / 1000.0
             launchHeight = float(FreeCAD.Units.Quantity(self._ui.launchSiteAltitudeInput.text())) / 1000.0
 
             temperatureUnits = self._ui.temperatureUnitsCombo.currentText()
