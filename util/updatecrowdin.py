c83dfe61bb2e31d5543a90713fb00228ab918699
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-10 04:40:57 -0500

    Use SPDX File Copyright Text

--- util/updatecrowdin.py
+++ util/updatecrowdin.py
@@ -1,30 +1,10 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# SPDX-File-Notice: Part of the Rocket addon.
+# SPDX-FileCopyrightText: 2015 Yorik van Havre <yorik@uncreated.net>
+# SPDX-FileCopyrightText: 2021 Benjamin Nauck <benjamin@nauck.se>
+# SPDX-FileCopyrightText: 2021 Mattias Pierre <github@mattiaspierre.com>
+# SPDX-File-Notice: Part of the FreeCAD project.
 
-# ***************************************************************************
-# *                                                                         *
-# *   Copyright (c) 2015 Yorik van Havre <yorik@uncreated.net>              *
-# *   Copyright (c) 2021 Benjamin Nauck <benjamin@nauck.se>                 *
-# *   Copyright (c) 2021 Mattias Pierre <github@mattiaspierre.com>          *
-# *                                                                         *
-# *   This file is part of FreeCAD.                                         *
-# *                                                                         *
-# *   FreeCAD is free software: you can redistribute it and/or modify it    *
-# *   under the terms of the GNU Lesser General Public License as           *
-# *   published by the Free Software Foundation, either version 2.1 of the  *
-# *   License, or (at your option) any later version.                       *
-# *                                                                         *
-# *   FreeCAD is distributed in the hope that it will be useful, but        *
-# *   WITHOUT ANY WARRANTY; without even the implied warranty of            *
-# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      *
-# *   Lesser General Public License for more details.                       *
-# *                                                                         *
-# *   You should have received a copy of the GNU Lesser General Public      *
-# *   License along with FreeCAD. If not, see                               *
-# *   <https://www.gnu.org/licenses/>.                                      *
-# *                                                                         *
-# ***************************************************************************
 
 """
 This utility offers several commands to interact with the FreeCAD project on
