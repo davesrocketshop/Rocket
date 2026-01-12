a840a11632af82c9bc78821e2f2ed2cc3af8cbf5
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-01-11 21:13:34 -0500

    Revert "Use SPDX File Copyright Text"
    This reverts commit c83dfe61bb2e31d5543a90713fb00228ab918699.

--- Rocket/cfd/parea.py
+++ Rocket/cfd/parea.py
@@ -1,14 +1,32 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# SPDX-FileCopyrightText: 2024 David Carter <dcarter@davidcarter.ca>
 # SPDX-File-Notice: Part of the Rocket addon.
 
-################################################################################
-#                                                                              #
-#   This code is heavily inspired by the parea Python module,                  #
-#   customized to work with FreeCAD meshes instead of STL files                #
-#   https://github.com/nathanrooy/p-area                                       #
-#                                                                              #
-################################################################################
+# ***************************************************************************
+# *   Copyright (c) 2024-2025 David Carter <dcarter@davidcarter.ca>         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+# *   This code is heavily inspired by the parea Python module, customized  *
+# *   to work with FreeCAD meshes instead of STL files                      *
+# *                                                                         *
+# *   https://github.com/nathanrooy/p-area                                  *
+# *                                                                         *
+# ***************************************************************************
 
 
 from shapely.geometry import Polygon
